4a1f2fdcca4fcb27d74b570c061c8e0a999fe76b
WilliamsAl282 <WilliamsAl282@northwested.org>
2024-09-18 10:23:54 -0400

    Did the work

---
+++ main.py
@@ -0,0 +1,13 @@
+# Alex Williams
+# 9/18/2024
+# IPO Intro
+
+num1 = input('enter a whole number:\n')
+num2 = input('Enter another whole number:\n')
+
+total = num1 + num2
+
+print(f'First number entered was: {num1}')
+print(f'Second number entered was: {num2}')
+print()
+print(f'The sum of {num1} and {num2} is:{total}')
