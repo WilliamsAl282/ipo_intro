5c237a8c25806762bce4519a8032945d8a1b9861
WilliamsAl282 <WilliamsAl282@northwested.org>
2024-09-18 10:31:35 -0400

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,20 @@ print(f'First number entered was: {num1}')
 print(f'Second number entered was: {num2}')
 print()
 print(f'The sum of {num1} and {num2} is:{total}')
+
+
+# num1 = input('enter a whole number:\n')
+# num1 = int(num1)
+# num1 = int(input('enter a whole number:\n'))
+num1 = float(input('Enter a decimal:\n'))
+
+# num2 = input('Enter another whole number:\n')
+# num2 = int(num2)
+# num2 = int(input('Enter another whole number:\n'))
+num2 = float(input('Enter another decimal:\n'))
+total = num1 + num2
+
+print(f'First number entered was: {num1}')
+print(f'Second number entered was: {num2}')
+print()
+print(f'The sum of {num1} and {num2} is:{total}')
